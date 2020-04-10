d5ad28b0fb1e8e163025c60b498590d91acc9158
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-04-10 11:58:12 +0400

    Добавлено модальное окно-корзина

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.close');
+const cancelButton = document.querySelector('.cancel');
+
+function toggleModal () {
+    modal.classList.toggle('is-open');
+}
+
+cartButton.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+cancelButton.addEventListener('click', toggleModal);
